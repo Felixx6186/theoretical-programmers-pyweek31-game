2e23fa8cd0cf6ec5267b1519f22ab059dbd9f538
soul crusher 2005 <ankithabhayan@gmail.com>
2021-03-28 13:06:01 +0530

    added a better loading screen with title
    background color is yellow at the moment with the title in black. the credit "by:- theoretical programmers" is in a lil grey colour. background picture is optional

--- main.py
+++ main.py
@@ -4,9 +4,16 @@ import sys
 pygame.init()
 screen = pygame.display.set_mode((800, 550))
 clock = pygame.time.Clock()
-font = pygame.font.SysFont("Arial", 40)
+font = pygame.font.SysFont("Monospace", 40)
 pygame.display.set_caption("cop running simulator - theoretical programmers")
 screen.fill((235, 225, 52))
+
+title_font = pygame.font.SysFont("Monospace", 45)
+credits_font = pygame.font.SysFont("Consolas", 20)
+title = title_font.render("Cop Running Simulator!", 1, (0,0,0))
+credit = credits_font.render("By:- Theoretical Programmers",1,(70,70,70))
+screen.blit(title, (100, 50))
+screen.blit(credit, (420, 105))
 
 class Button:
     "Create a button, then blit the surface in the while loop"
@@ -17,15 +24,15 @@ class Button:
         self.change_text(text, bg)
 
     def change_text(self, text, bg="black"):
-        self.text = font.render(text, 1, pygame.Color("White"))
+        self.text = font.render(text, 1, pygame.Color("Black"))
         self.size = self.text.get_size()
         self.surface = pygame.Surface(self.size)
         self.surface.fill(bg)
         self.surface.blit(self.text, (0, 0))
         self.rect = pygame.Rect(self.x, self.y, self.size[0], self.size[1])
 
-start_button = Button("Start", x=365, y=200, bg="green")
-end_button = Button("Quit",x=370,y=280, bg="red")
+start_button = Button("Start", x=338, y=220, bg="green")
+end_button = Button("Quit",x=350,y=300, bg="red")
 
 def show_buttons():
     screen.blit(start_button.surface, (start_button.x, start_button.y))
@@ -49,7 +56,7 @@ while loop:
         if event.type == pygame.QUIT:
             loop = 0
         button1_click(event)
-    pygame.draw.rect(screen, (0, 0, 0), (250, 160, 300, 200))
+    pygame.draw.rect(screen, (0, 0, 0), (215, 200, 370,170))
     show_buttons()
     pygame.display.update()
     clock.tick(30)

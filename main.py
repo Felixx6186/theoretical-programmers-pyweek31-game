8a0ffc6cc773462ad2de280fd40f64797c5ab568
soul crusher 2005 <ankithabhayan@gmail.com>
2021-03-28 14:23:16 +0530

    cleaned up some code.
    code is super nice now.

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import pygame
 import sys
 
+#initializing window with yellow colour and window title
 pygame.init()
 screen = pygame.display.set_mode((800, 550))
-clock = pygame.time.Clock()
 font = pygame.font.SysFont("Monospace", 40)
 pygame.display.set_caption("cop running simulator - theoretical programmers")
 screen.fill((235, 225, 52))
 
+#main title and credits
 title_font = pygame.font.SysFont("Monospace", 45)
 credits_font = pygame.font.SysFont("Consolas", 20)
 title = title_font.render("Cop Running Simulator!", 1, (0,0,0))
@@ -15,9 +16,8 @@ credit = credits_font.render("By:- Theoretical Programmers",1,(70,70,70))
 screen.blit(title, (100, 50))
 screen.blit(credit, (420, 105))
 
+#button class to easily create buttons and manage them
 class Button:
-    "Create a button, then blit the surface in the while loop"
-
     def __init__(self, text,  x, y, bg="black"):
         self.x = x
         self.y = y
@@ -31,14 +31,14 @@ class Button:
         self.surface.blit(self.text, (0, 0))
         self.rect = pygame.Rect(self.x, self.y, self.size[0], self.size[1])
 
+#start and quit buttons under a rectangular black surface
+pygame.draw.rect(screen, (0, 0, 0), (215, 200, 370,170))
 start_button = Button("Start", x=338, y=220, bg="green")
 end_button = Button("Quit",x=350,y=300, bg="red")
+screen.blit(start_button.surface, (start_button.x, start_button.y))
+screen.blit(end_button.surface,(end_button.x,end_button.y))
 
-def show_buttons():
-    screen.blit(start_button.surface, (start_button.x, start_button.y))
-    screen.blit(end_button.surface,(end_button.x,end_button.y))
-
-
+#handling mouse click events
 def button1_click(event):
     x, y = pygame.mouse.get_pos()
     if event.type == pygame.MOUSEBUTTONDOWN:
@@ -49,16 +49,10 @@ def button1_click(event):
             elif end_button.rect.collidepoint(x,y):
                 sys.exit()
 
-
-loop = 1
-while loop:
+#main loop
+while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            loop = 0
+            sys.exit()
         button1_click(event)
-    pygame.draw.rect(screen, (0, 0, 0), (215, 200, 370,170))
-    show_buttons()
     pygame.display.update()
-    clock.tick(30)
-
-pygame.quit()
